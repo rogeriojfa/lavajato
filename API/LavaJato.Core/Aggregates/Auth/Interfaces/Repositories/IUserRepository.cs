e9ecbced5fa6c38070027744d77fc67c38d3bdb2
gabrielrigolon <gabrielrigolon@gmail.com>
2021-10-05 02:47:59 -0300

    Criado projeto do backend. LavaJato.API utilizando .NET Core 5 e feita uma previa da organizacao das pastas

---
+++ API/LavaJato.Core/Aggregates/Auth/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using GestaoEventos.Core.Aggregates.AuthAgg.Dtos;
+using GestaoEventos.Core.Aggregates.AuthAgg.Entities;
+using GestaoEventos.Core.SharedKernel.Dtos;
+using GestaoEventos.Core.SharedKernel.Interfaces.Repositories;
+
+namespace GestaoEventos.Core.Aggregates.AuthAgg.Interfaces.Repositories
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        User GetByLogin(string loginUser);
+        IEnumerable<int> GetUsersCells();
+        FilterGenericDto<UserForGetDto> FilterUsers(UserParametersDto userParametersDto);
+    }
+}
